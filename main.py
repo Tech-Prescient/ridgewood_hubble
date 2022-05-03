1fcf642547ea36e8cd3130f935ffcec01e80b58c
Utkarsh Srivastava <104355212+ut-techprescient@users.noreply.github.com>
2022-05-03 18:55:17 +0530

    Uncomment main function

--- main.py
+++ main.py
@@ -33,14 +33,14 @@ def main(args):
             }
 
     seller_ids = ["BTLUS", "Cube"]
-    # with MySqlConnection(**conn_str) as source_connection, MySqlConnection(**dest_conn_str) as destination_connection:
-    #     # Migrations for the tables
-    #     table_migration = MySqlTableMigration(destination_connection.connection)
-    #     table_migration.execute()
+    with MySqlConnection(**conn_str) as source_connection, MySqlConnection(**dest_conn_str) as destination_connection:
+        # Migrations for the tables
+        table_migration = MySqlTableMigration(destination_connection.connection)
+        table_migration.execute()
 
-    #     # Migrations for the data
-    #     migrations = MySqlDataMigration(source_connection, destination_connection)
-    #     migrations.execute(seller_ids)
+        # Migrations for the data
+        migrations = MySqlDataMigration(source_connection, destination_connection)
+        migrations.execute(seller_ids)
         
 
 
